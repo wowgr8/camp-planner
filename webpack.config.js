6c282a14a05582665b1088e927613be1a4444c28
John Whitten <johnwhitten.studio@gmail.com>
2022-01-19 20:31:55 -0800

    update webpack to handle fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
@@ -9,34 +9,45 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'eval-source-map',
-  devServer: {                
-    contentBase: './dist'      
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist"
   },
-  plugins: [
+  plugins: [ 
     new Dotenv(),
-    new CleanWebpackPlugin(), 
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'currency-exchange-rate-API',
+      title: 'camp-planner',
       template: './src/index.html',
       inject: 'body'
     })
   ],
-  module: {
+  module: { 
     rules: [
       {
-        test: /\.(gif|png|jpe?g)$/,
+        test: /\.(svg|ico|gif|png|jpe?g)$/,
         use: [
           {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'assets/images/'
+              outputPath: 'assets/'
             }
           }
         ]
       },
-      
+      {
+        test: /\.(woff(2)?|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/'
+            }
+          }
+        ]
+      },
       {
         test:/\.html$/,
         use: [
