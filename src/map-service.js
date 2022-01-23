997c2ced6a37ab8e51b77c9c05a565b5b54e01ec
Christen Weston <paleeserecycle@gmail.com>
2022-01-22 17:49:20 -0800

    Created initial map-service

---
+++ src/map-service.js
@@ -0,0 +1,14 @@
+export default class MapService {
+  static getMap(lat,lon) {
+    return fetch(`https://maps.locationiq.com/v3/staticmap?key=${process.env.API_KEY_MAP}&center=${lat},${lon}&zoom=10`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return `https://maps.locationiq.com/v3/staticmap?key=${process.env.API_KEY_MAP}&center=${lat},${lon}&zoom=10`;
+      })
+      .catch(function(error) {
+        return Error(error);
+      });
+  }
+}
