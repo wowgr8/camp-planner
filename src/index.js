e803054da26107875618c5f79ea6a77f14f1ef6c
Christen Weston <paleeserecycle@gmail.com>
2022-01-22 17:50:06 -0800

    It's a sloppy mess but weather and map are in action

--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@
  import 'bootstrap/dist/css/bootstrap.min.css';
  import './css/styles.css';
  import WeatherService from './weather-service.js';
+ import MapService from './map-service.js';
+
+ function forecastByDay(response) {
+  let forcastDateAndTime = "";
+ // let dateArray = [];
+ // let dateArrayNice;
+  for (let i = 0; i<response.list.length; i++) {
+   // let dateToForecast = new Date((response.list[i].dt_txt));
+   // let dateToForecastNice = dateToForecast.toDateString();
+   // dateArray.push(dateToForecastNice);
+    //dateArrayNice = new Set(dateArray);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric' };
+    let weatherIcon = response.list[i].weather[0].icon;
+    let timeStampToNiceFormat = (new Date((response.list[i].dt_txt))).toLocaleDateString('en-US', options);
+    forcastDateAndTime += `<li>${timeStampToNiceFormat}: Max Temp: ${response.list[i].main.temp_max} 
+    Min Temp: ${response.list[i].main.temp_min}  <img src="${`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`}" alt="weather image"></li>`;
+    //console.log(dateToForecastNice + response.list[i].main.temp_min);
+  }
+  //forcastDateAndTime += `<li>${dateArrayNice}</li>`;
+  $('.forecast-list').html(forcastDateAndTime);
+  //console.log(dateArrayNice);
+  return 0;
+}
 
 function displayResults(response) {
   if(response.list) {
     console.log("It's weather time!");
+    (forecastByDay(response));
   } else {
     console.log("No weather for you");
   }
@@ -15,10 +39,19 @@ function displayResults(response) {
  $(document).ready(function() {
   $('.main_nav').click(function(event) {
     event.preventDefault();
+    let lat, lon;
 
     WeatherService.getWeatherForecast()
     .then(function(response) {
       displayResults(response);
+      lat = response.city.coord.lat;
+      lon = response.city.coord.lon;
+      console.log("This is the lat: " + lat + " lon: " + lon);
+      //This seems like a bad idea to call another service from w/in weather service, but here we are!
+      MapService.getMap(lat,lon)
+      .then(function(mapResponse) {
+        $(".map").html(`<img src="${mapResponse}" class="img-fluid">`);
+      });
     });
   });
  });
--- src/weather-service.js
+++ src/weather-service.js
@@ -2,17 +2,20 @@
 //api.openweathermap.org/data/2.5/forecast?appid=7f2804a37e3a228a08d8c5003214d84d&q=97206
 
 export default class WeatherService {
+  // constructor() {
+  //   maxTemp = this.calcMaxTemp();
+  //   minTemp = this.calcMinTemp();
+  // }
   //static getWeatherForecast(zipcode){}
   static getWeatherForecast() {
-    //Note: It could be zipcode or city/state etc. The query area is pretty flexible. Fetch below just for testing with 97206 zip
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?appid=${process.env.API_KEY_WEATHER}&q=97206`)
+    //Note: It could be zipcode or city/state etc, but city state seems more reliable. Fetch below just for testing with Portland,Oregon
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?appid=${process.env.API_KEY_WEATHER}&q=Portland,Oregon&country=US&units=imperial`)
     //return fetch(`/api.openweathermap.org/data/2.5/forecast?appid=${process.env.API_KEY}&q=${zipcode}`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
         else if (response.ok && response.status === 200){
-          //console.log("response.json");
           return response.json();
         }
         else {
@@ -23,4 +26,18 @@ export default class WeatherService {
         return error;
       });
   }
+  //  calcMinTemp() {
+  //     let minTemp = 100;
+  //     for(let hourlyInterval = 0; hourlyInterval<=8; hourlyInterval++) {
+  //       if (response.list[i].main.temp_max > maxTemp) {
+  //         maxTemp = response.list[i].main.temp_max;
+  //       }
+  //       if (response.list[i].main.temp_min < minTemp) {
+  //         minTemp = response.list[i].main.temp_min;
+  //       }
+  //       else {
+  //         console.log("No change to temp max and min");
+  //       }
+  //     }
+  //   }
 }
